8e35d429fc065b284cb940a6c662ef244accef3a
Nikita Koselev <koselev@gmail.com>
2023-01-07 23:49:24 +0000

    Initial commit

---
+++ expenses.go
@@ -0,0 +1,75 @@
+package expenses
+
+import "errors"
+
+// Record represents an expense record.
+type Record struct {
+	Day      int
+	Amount   float64
+	Category string
+}
+
+// DaysPeriod represents a period of days for expenses.
+type DaysPeriod struct {
+	From int
+	To   int
+}
+
+// Filter returns the records for which the predicate function returns true.
+func Filter(in []Record, predicate func(Record) bool) []Record {
+	var out []Record
+
+	for _, r := range in {
+		if predicate(r) {
+			out = append(out, r)
+		}
+	}
+
+	return out
+}
+
+// ByDaysPeriod returns predicate function that returns true when
+// the day of the record is inside the period of day and false otherwise.
+func ByDaysPeriod(p DaysPeriod) func(Record) bool {
+	return func(r Record) bool {
+		if r.Day >= p.From && r.Day <= p.To {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+// returns predicate function that returns true when
+// the category of the record is the same as the provided category
+// and false otherwise.
+func ByCategory(c string) func(Record) bool {
+	return func(r Record) bool {
+		return r.Category == c
+	}
+}
+
+// TotalByPeriod returns total amount of expenses for records
+// inside the period p.
+func TotalByPeriod(in []Record, p DaysPeriod) float64 {
+	var result float64
+
+	for _, r := range in {
+		if r.Day >= p.From && r.Day <= p.To {
+			result += r.Amount
+		}
+	}
+	return result
+}
+
+// CategoryExpenses returns total amount of expenses for records
+// in category c that are also inside the period p.
+// An error must be returned only if there are no records in the list that belong
+// to the given category, regardless of period of time.
+func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
+	filteredByCategory := Filter(in, ByCategory(c))
+	if len(filteredByCategory) == 0 {
+		return 0, errors.New("unknown category entertainment")
+	}
+	return TotalByPeriod(filteredByCategory, p), nil
+}
